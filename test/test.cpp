cb85707642cf7ca436106a46ad626a480f865b02
frostbane <frostbane@programmer.net>
2021-09-19 03:23:19 +1000

    add CMakeLists.txt and format sources

--- test/test.cpp
+++ test/test.cpp
@@ -31,7 +31,7 @@ using namespace tthread;
 
 // HACK: Mac OS X and early MinGW do not support thread-local storage
 #if defined(__APPLE__) || (defined(__MINGW32__) && (__GNUC__ < 4))
- #define NO_TLS
+#   define NO_TLS
 #endif
 
 
@@ -41,327 +41,328 @@ thread_local int gLocalVar;
 #endif
 
 // Mutex + global count variable
-mutex gMutex;
-fast_mutex gFastMutex;
+mutex       gMutex;
+fast_mutex  gFastMutex;
 atomic_flag gFlag(ATOMIC_FLAG_INIT);
-int gCount;
-atomic_int gAtomicCount;
+int         gCount;
+atomic_int  gAtomicCount;
 
 // Condition variable
 condition_variable gCond;
 
 // Thread function: Thread ID
-void ThreadIDs(void * aArg)
+void
+ThreadIDs(void* aArg)
 {
-  cout << " My thread id is " << this_thread::get_id() << "." << endl;
+    cout << " My thread id is " << this_thread::get_id() << "." << endl;
 }
 
 #ifndef NO_TLS
+
 // Thread function: Thread-local storage
-void ThreadTLS(void * aArg)
+void
+ThreadTLS(void* aArg)
 {
-  gLocalVar = 2;
-  cout << " My gLocalVar is " << gLocalVar << "." << endl;
+    gLocalVar = 2;
+    cout << " My gLocalVar is " << gLocalVar << "." << endl;
 }
+
 #endif
 
 // Thread function: Mutex locking
-void ThreadLock(void * aArg)
+void
+ThreadLock(void* aArg)
 {
-  for(int i = 0; i < 10000; ++ i)
-  {
-    lock_guard<mutex> lock(gMutex);
-    ++ gCount;
-  }
+    for (int i = 0; i < 10000; ++i) {
+        lock_guard<mutex> lock(gMutex);
+        ++gCount;
+    }
 }
 
 // Thread function: Fast mutex locking
-void ThreadLock2(void * aArg)
+void
+ThreadLock2(void* aArg)
 {
-  for(int i = 0; i < 10000; ++ i)
-  {
-    lock_guard<fast_mutex> lock(gFastMutex);
-    ++ gCount;
-  }
+    for (int i = 0; i < 10000; ++i) {
+        lock_guard<fast_mutex> lock(gFastMutex);
+        ++gCount;
+    }
 }
 
 // Thread function: Spin-lock locking
-void ThreadSpinLock(void * aArg)
+void
+ThreadSpinLock(void* aArg)
 {
-  for(int i = 0; i < 10000; ++ i)
-  {
-    // CPU-friendly spin-lock
-    while (gFlag.test_and_set())
-      this_thread::yield();
+    for (int i = 0; i < 10000; ++i) {
+        // CPU-friendly spin-lock
+        while (gFlag.test_and_set())
+            this_thread::yield();
 
-    ++ gCount;
+        ++gCount;
 
-    // Release lock
-    gFlag.clear();
-  }
+        // Release lock
+        gFlag.clear();
+    }
 }
 
 // Thread function: Atomic count
-void ThreadAtomicCount(void * aArg)
+void
+ThreadAtomicCount(void* aArg)
 {
-  for(int i = 0; i < 10000; ++ i)
-  {
-    ++ gAtomicCount;
-  }
+    for (int i = 0; i < 10000; ++i) {
+        ++gAtomicCount;
+    }
 }
 
 // Thread function: Condition notifier
-void ThreadCondition1(void * aArg)
+void
+ThreadCondition1(void* aArg)
 {
-  lock_guard<mutex> lock(gMutex);
-  -- gCount;
-  gCond.notify_all();
+    lock_guard<mutex> lock(gMutex);
+    --gCount;
+    gCond.notify_all();
 }
 
 // Thread function: Condition waiter
-void ThreadCondition2(void * aArg)
+void
+ThreadCondition2(void* aArg)
 {
-  cout << " Wating..." << flush;
-  lock_guard<mutex> lock(gMutex);
-  while(gCount > 0)
-  {
-    cout << "." << flush;
-    gCond.wait(gMutex);
-  }
-  cout << "." << endl;
+    cout << " Wating..." << flush;
+    lock_guard<mutex> lock(gMutex);
+    while (gCount > 0) {
+        cout << "." << flush;
+        gCond.wait(gMutex);
+    }
+    cout << "." << endl;
 }
 
 // Thread function: Yield
-void ThreadYield(void * aArg)
+void
+ThreadYield(void* aArg)
 {
-  // Yield...
-  this_thread::yield();
+    // Yield...
+    this_thread::yield();
 }
 
 // Thread function: Detach
-void ThreadDetach(void * aArg)
+void
+ThreadDetach(void* aArg)
 {
-  // We don't do anything much, just sleep a little...
-  this_thread::sleep_for(chrono::milliseconds(100));
-  cout << " Detached thread finished." << endl;
+    // We don't do anything much, just sleep a little...
+    this_thread::sleep_for(chrono::milliseconds(100));
+    cout << " Detached thread finished." << endl;
 }
 
 
 // This is the main program (i.e. the main thread)
-int main()
+int
+main()
 {
-  // Test 1: Show number of CPU cores in the system
-  cout << "PART I: Info" << endl;
-  cout << " Number of processor cores: " << thread::hardware_concurrency() << endl;
+    // Test 1: Show number of CPU cores in the system
+    cout << "PART I: Info" << endl;
+    cout << " Number of processor cores: " << thread::hardware_concurrency() << endl;
 
-  // Test 2: thread IDs
-  cout << endl << "PART II: Thread IDs" << endl;
-  {
-    // Show the main thread ID
-    cout << " Main thread id is " << this_thread::get_id() << "." << endl;
+    // Test 2: thread IDs
+    cout << endl << "PART II: Thread IDs" << endl;
+    {
+        // Show the main thread ID
+        cout << " Main thread id is " << this_thread::get_id() << "." << endl;
 
-    // Start a bunch of child threads - only run a single thread at a time
-    thread t1(ThreadIDs, 0);
-    t1.join();
-    thread t2(ThreadIDs, 0);
-    t2.join();
-    thread t3(ThreadIDs, 0);
-    t3.join();
-    thread t4(ThreadIDs, 0);
-    t4.join();
-  }
+        // Start a bunch of child threads - only run a single thread at a time
+        thread t1(ThreadIDs, 0);
+        t1.join();
+        thread t2(ThreadIDs, 0);
+        t2.join();
+        thread t3(ThreadIDs, 0);
+        t3.join();
+        thread t4(ThreadIDs, 0);
+        t4.join();
+    }
 
-  // Test 3: thread local storage
-  cout << endl << "PART III: Thread local storage" << endl;
+    // Test 3: thread local storage
+    cout << endl << "PART III: Thread local storage" << endl;
 #ifndef NO_TLS
-  {
-    // Clear the TLS variable (it should keep this value after all threads are
-    // finished).
-    gLocalVar = 1;
-    cout << " Main gLocalVar is " << gLocalVar << "." << endl;
+    {
+        // Clear the TLS variable (it should keep this value after all threads are
+        // finished).
+        gLocalVar = 1;
+        cout << " Main gLocalVar is " << gLocalVar << "." << endl;
 
-    // Start a child thread that modifies gLocalVar
-    thread t1(ThreadTLS, 0);
-    t1.join();
+        // Start a child thread that modifies gLocalVar
+        thread t1(ThreadTLS, 0);
+        t1.join();
 
-    // Check if the TLS variable has changed
-    if(gLocalVar == 1)
-      cout << " Main gLocalID was not changed by the child thread - OK!" << endl;
-    else
-      cout << " Main gLocalID was changed by the child thread - FAIL!" << endl;
-  }
+        // Check if the TLS variable has changed
+        if (gLocalVar == 1)
+            cout << " Main gLocalID was not changed by the child thread - OK!" << endl;
+        else
+            cout << " Main gLocalID was changed by the child thread - FAIL!" << endl;
+    }
 #else
-  cout << " TLS is not supported on this platform..." << endl;
+    cout << " TLS is not supported on this platform..." << endl;
 #endif
 
-  // Test 4: mutex locking
-  cout << endl << "PART IV: Mutex locking (100 threads x 10000 iterations)" << endl;
-  {
-    // Clear the global counter.
-    gCount = 0;
-
-    // Start a bunch of child threads
-    list<thread *> threadList;
-    for(int i = 0; i < 100; ++ i)
-      threadList.push_back(new thread(ThreadLock, 0));
-
-    // Wait for the threads to finish
-    list<thread *>::iterator it;
-    for(it = threadList.begin(); it != threadList.end(); ++ it)
+    // Test 4: mutex locking
+    cout << endl << "PART IV: Mutex locking (100 threads x 10000 iterations)" << endl;
     {
-      thread * t = *it;
-      t->join();
-      delete t;
+        // Clear the global counter.
+        gCount          = 0;
+
+        // Start a bunch of child threads
+        list<thread*> threadList;
+        for (int      i = 0; i < 100; ++i)
+            threadList.push_back(new thread(ThreadLock, 0));
+
+        // Wait for the threads to finish
+        list<thread*>::iterator it;
+        for (it = threadList.begin(); it != threadList.end(); ++it) {
+            thread* t = *it;
+            t->join();
+            delete t;
+        }
+
+        // Check the global count
+        cout << " gCount = " << gCount << endl;
     }
 
-    // Check the global count
-    cout << " gCount = " << gCount << endl;
-  }
-
-  // Test 5: fast_mutex locking
-  cout << endl << "PART V: Fast mutex locking (100 threads x 10000 iterations)" << endl;
-  {
-    // Clear the global counter.
-    gCount = 0;
-
-    // Start a bunch of child threads
-    list<thread *> threadList;
-    for(int i = 0; i < 100; ++ i)
-      threadList.push_back(new thread(ThreadLock2, 0));
-
-    // Wait for the threads to finish
-    list<thread *>::iterator it;
-    for(it = threadList.begin(); it != threadList.end(); ++ it)
+    // Test 5: fast_mutex locking
+    cout << endl << "PART V: Fast mutex locking (100 threads x 10000 iterations)" << endl;
     {
-      thread * t = *it;
-      t->join();
-      delete t;
+        // Clear the global counter.
+        gCount          = 0;
+
+        // Start a bunch of child threads
+        list<thread*> threadList;
+        for (int      i = 0; i < 100; ++i)
+            threadList.push_back(new thread(ThreadLock2, 0));
+
+        // Wait for the threads to finish
+        list<thread*>::iterator it;
+        for (it = threadList.begin(); it != threadList.end(); ++it) {
+            thread* t = *it;
+            t->join();
+            delete t;
+        }
+
+        // Check the global count
+        cout << " gCount = " << gCount << endl;
     }
 
-    // Check the global count
-    cout << " gCount = " << gCount << endl;
-  }
-
-  // Test 6: Atomic lock
-  cout << endl << "PART VI: Atomic spin-lock (100 threads x 10000 iterations)" << endl;
-  {
-    // Clear the global counter.
-    gCount = 0;
-
-    // Start a bunch of child threads
-    list<thread *> threadList;
-    for(int i = 0; i < 100; ++ i)
-      threadList.push_back(new thread(ThreadSpinLock, 0));
-
-    // Wait for the threads to finish
-    list<thread *>::iterator it;
-    for(it = threadList.begin(); it != threadList.end(); ++ it)
+    // Test 6: Atomic lock
+    cout << endl << "PART VI: Atomic spin-lock (100 threads x 10000 iterations)" << endl;
     {
-      thread * t = *it;
-      t->join();
-      delete t;
+        // Clear the global counter.
+        gCount          = 0;
+
+        // Start a bunch of child threads
+        list<thread*> threadList;
+        for (int      i = 0; i < 100; ++i)
+            threadList.push_back(new thread(ThreadSpinLock, 0));
+
+        // Wait for the threads to finish
+        list<thread*>::iterator it;
+        for (it = threadList.begin(); it != threadList.end(); ++it) {
+            thread* t = *it;
+            t->join();
+            delete t;
+        }
+
+        // Check the global count
+        cout << " gCount = " << gCount << endl;
     }
 
-    // Check the global count
-    cout << " gCount = " << gCount << endl;
-  }
-
-  // Test 7: Atomic variable
-  cout << endl << "PART VII: Atomic variable (100 threads x 10000 iterations)" << endl;
-  {
-    // Clear the global counter.
-    gAtomicCount = 0;
-
-    // Start a bunch of child threads
-    list<thread *> threadList;
-    for(int i = 0; i < 100; ++ i)
-      threadList.push_back(new thread(ThreadAtomicCount, 0));
-
-    // Wait for the threads to finish
-    list<thread *>::iterator it;
-    for(it = threadList.begin(); it != threadList.end(); ++ it)
+    // Test 7: Atomic variable
+    cout << endl << "PART VII: Atomic variable (100 threads x 10000 iterations)" << endl;
     {
-      thread * t = *it;
-      t->join();
-      delete t;
+        // Clear the global counter.
+        gAtomicCount    = 0;
+
+        // Start a bunch of child threads
+        list<thread*> threadList;
+        for (int      i = 0; i < 100; ++i)
+            threadList.push_back(new thread(ThreadAtomicCount, 0));
+
+        // Wait for the threads to finish
+        list<thread*>::iterator it;
+        for (it = threadList.begin(); it != threadList.end(); ++it) {
+            thread* t = *it;
+            t->join();
+            delete t;
+        }
+
+        // Check the global count
+        cout << " gAtomicCount = " << gAtomicCount << endl;
     }
 
-    // Check the global count
-    cout << " gAtomicCount = " << gAtomicCount << endl;
-  }
-
-  // Test 8: condition variable
-  cout << endl << "PART VIII: Condition variable (40 + 1 threads)" << endl;
-  {
-    // Set the global counter to the number of threads to run.
-    gCount = 40;
-
-    // Start the waiting thread (it will wait for gCount to reach zero).
-    thread t1(ThreadCondition2, 0);
-
-    // Start a bunch of child threads (these will decrease gCount by 1 when they
-    // finish)
-    list<thread *> threadList;
-    for(int i = 0; i < 40; ++ i)
-      threadList.push_back(new thread(ThreadCondition1, 0));
-
-    // Wait for the waiting thread to finish
-    t1.join();
-
-    // Wait for the other threads to finish
-    list<thread *>::iterator it;
-    for(it = threadList.begin(); it != threadList.end(); ++ it)
+    // Test 8: condition variable
+    cout << endl << "PART VIII: Condition variable (40 + 1 threads)" << endl;
     {
-      thread * t = *it;
-      t->join();
-      delete t;
+        // Set the global counter to the number of threads to run.
+        gCount          = 40;
+
+        // Start the waiting thread (it will wait for gCount to reach zero).
+        thread t1(ThreadCondition2, 0);
+
+        // Start a bunch of child threads (these will decrease gCount by 1 when they
+        // finish)
+        list<thread*> threadList;
+        for (int      i = 0; i < 40; ++i)
+            threadList.push_back(new thread(ThreadCondition1, 0));
+
+        // Wait for the waiting thread to finish
+        t1.join();
+
+        // Wait for the other threads to finish
+        list<thread*>::iterator it;
+        for (it = threadList.begin(); it != threadList.end(); ++it) {
+            thread* t = *it;
+            t->join();
+            delete t;
+        }
     }
-  }
 
-  // Test 9: yield
-  cout << endl << "PART IX: Yield (40 + 1 threads)" << endl;
-  {
-    // Start a bunch of child threads
-    list<thread *> threadList;
-    for(int i = 0; i < 40; ++ i)
-      threadList.push_back(new thread(ThreadYield, 0));
-
-    // Yield...
-    this_thread::yield();
-
-    // Wait for the threads to finish
-    list<thread *>::iterator it;
-    for(it = threadList.begin(); it != threadList.end(); ++ it)
+    // Test 9: yield
+    cout << endl << "PART IX: Yield (40 + 1 threads)" << endl;
     {
-      thread * t = *it;
-      t->join();
-      delete t;
-    }
-  }
+        // Start a bunch of child threads
+        list<thread*> threadList;
+        for (int      i = 0; i < 40; ++i)
+            threadList.push_back(new thread(ThreadYield, 0));
 
-  // Test 10: sleep
-  cout << endl << "PART X: Sleep (10 x 100 ms)" << endl;
-  {
-    // Sleep...
-    cout << " Sleeping" << flush;
-    for(int i = 0; i < 10; ++ i)
+        // Yield...
+        this_thread::yield();
+
+        // Wait for the threads to finish
+        list<thread*>::iterator it;
+        for (it = threadList.begin(); it != threadList.end(); ++it) {
+            thread* t = *it;
+            t->join();
+            delete t;
+        }
+    }
+
+    // Test 10: sleep
+    cout << endl << "PART X: Sleep (10 x 100 ms)" << endl;
     {
-      this_thread::sleep_for(chrono::milliseconds(100));
-      cout << "." << flush;
+        // Sleep...
+        cout << " Sleeping" << flush;
+        for (int i = 0; i < 10; ++i) {
+            this_thread::sleep_for(chrono::milliseconds(100));
+            cout << "." << flush;
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
 
-  // Test 11: detach
-  cout << endl << "PART XI: Detach" << endl;
-  {
-    thread t(ThreadDetach, 0);
-    t.detach();
-    cout << " Detached from thread." << endl;
+    // Test 11: detach
+    cout << endl << "PART XI: Detach" << endl;
+    {
+        thread t(ThreadDetach, 0);
+        t.detach();
+        cout << " Detached from thread." << endl;
 
-    // Give the thread a chanse to finish too...
-    this_thread::sleep_for(chrono::milliseconds(400));
-  }
+        // Give the thread a chanse to finish too...
+        this_thread::sleep_for(chrono::milliseconds(400));
+    }
 
-  cout << endl << "Tests done!" << endl;
+    cout << endl << "Tests done!" << endl;
 }
